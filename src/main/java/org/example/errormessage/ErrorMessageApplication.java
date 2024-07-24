fb20c711de0b79f21affa6f5af5b60e5e87e1b09
Pourya <pourya.nejadensan@gmail.com>
2024-07-24 10:55:14 +0200

    Initial commit

---
+++ src/main/java/org/example/errormessage/ErrorMessageApplication.java
@@ -0,0 +1,13 @@
+package org.example.errormessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrorMessageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/errormessage/ErrorMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.errormessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
